e79bd07b182c377093c152acaf3893d79a0eb643
Krishna Kumar <3963513+kks32@users.noreply.github.com>
2018-10-16 16:28:25 +0100

    :construction: WIP moving to router class

--- src/main.cc
+++ src/main.cc
@@ -4,10 +4,8 @@
 #include <chrono>
 #include <ctime>
 #include <memory>
-#include <numeric>
 
 #include "graph.h"
-#include "mpi_wrapper.h"
 #include "router.h"
 
 int main(int argc, char** argv) {
@@ -29,66 +27,14 @@ int main(int argc, char** argv) {
     MPI_Abort(MPI_COMM_WORLD, 1);
   }
 
-  // Vector of all origin destination pairs from router
-  std::vector<std::array<abm::graph::vertex_t, 2>> all_od_pairs;
-
   // On MPI rank 0 create a router and fetch all OD pairs
+  std::shared_ptr<abm::Router> router;
   if (mpi_rank == 0) {
-    auto router = std::make_unique<abm::Router>(5000, graph);
-    router->read_od_pairs(od_file);
-
-    all_od_pairs = router->od_pairs();
-    all_od_pairs.resize(5000);
+    router = std::make_shared<abm::Router>(graph);
+    router->read_od_pairs(od_file, 5000);
   }
 
-  // Create MPI pair type
-  MPI_Datatype pair_t;
-  MPI_Type_vector(2, 1, 1, MPI_INT, &pair_t);
-  MPI_Type_commit(&pair_t);
-
-  // Get number of MPI ranks
-  int mpi_size;
-  MPI_Comm_size(MPI_COMM_WORLD, &mpi_size);
-
-  // Calculate chunk size to split router
-  int chunk_size = all_od_pairs.size() / mpi_size;
-  MPI_Bcast(&chunk_size, 1, MPI_INT, 0, MPI_COMM_WORLD);
-  std::vector<std::array<abm::graph::vertex_t, 2>> od_pairs(chunk_size);
-
-  // Send route chunks to different compute nodes
-  MPI_Scatter(all_od_pairs.data(), chunk_size, pair_t, od_pairs.data(),
-              od_pairs.size(), pair_t, 0, MPI_COMM_WORLD);
-  // Calculate the remaining chunk of od_pairs and add to rank 0
-  int chunk_remainder = all_od_pairs.size() % mpi_size;
-  if (mpi_rank == 0) {
-    od_pairs.insert(od_pairs.begin(), all_od_pairs.end() - chunk_remainder,
-                    all_od_pairs.end());
-  }
-
-  // Paths (vector of edges)
-  std::vector<std::array<abm::graph::vertex_t, 2>> paths;
-  paths.reserve(graph->nedges());
-
-  // Indices of start of path and length for each agent
-  std::vector<std::array<abm::graph::vertex_t, 3>> paths_idx;
-  paths_idx.reserve(od_pairs.size());
-
-#pragma omp parallel for schedule(dynamic)
-  for (abm::graph::vertex_t i = 0; i < od_pairs.size(); ++i) {
-    const auto sp = graph->dijkstra(od_pairs[i][0], od_pairs[i][1]);
-
-#pragma omp critical
-    {
-      paths_idx.emplace_back(std::array<abm::graph::vertex_t, 3>(
-          {i, static_cast<abm::graph::vertex_t>(paths.size()),
-           static_cast<abm::graph::vertex_t>(sp.size())}));
-      paths.insert(std::end(paths), std::begin(sp), std::end(sp));
-    }
-  }
-
-  // Get all paths and indices
-  const auto all_paths = abm::gather_vector_arrays(paths);
-  auto const all_paths_idx = abm::gather_vector_arrays(paths_idx);
+  const auto all_paths = router->compute_routes(mpi_rank);
 
   if (mpi_rank == 0)
     std::cout << "Collected paths: " << all_paths.size() << std::endl;
--- src/router.cc
+++ src/router.cc
@@ -1,7 +1,7 @@
 #include "router.h"
 
 // Read OD pairs file format
-bool abm::Router::read_od_pairs(const std::string& filename) {
+bool abm::Router::read_od_pairs(const std::string& filename, int nagents) {
   bool status = true;
   try {
     io::CSVReader<2> in(filename);
@@ -10,11 +10,67 @@ bool abm::Router::read_od_pairs(const std::string& filename) {
     double weight;
     while (in.read_row(v1, v2)) {
       std::array<abm::graph::vertex_t, 2> od = {v1, v2};
-      this->od_pairs_.emplace_back(od);
+      this->all_od_pairs_.emplace_back(od);
     }
+    if (nagents != std::numeric_limits<int>::max())
+      all_od_pairs_.resize(nagents);
   } catch (std::exception& exception) {
     std::cout << "Read OD file: " << exception.what() << "\n";
     status = false;
   }
   return status;
+}
+
+std::vector<std::array<abm::graph::vertex_t, 2>> abm::Router::compute_routes(
+    int mpi_rank) {
+  // Create MPI pair type
+  MPI_Datatype pair_t;
+  MPI_Type_vector(2, 1, 1, MPI_INT, &pair_t);
+  MPI_Type_commit(&pair_t);
+
+  // Get number of MPI ranks
+  int mpi_size;
+  MPI_Comm_size(MPI_COMM_WORLD, &mpi_size);
+
+  // Calculate chunk size to split router
+  int chunk_size = this->all_od_pairs_.size() / mpi_size;
+  MPI_Bcast(&chunk_size, 1, MPI_INT, 0, MPI_COMM_WORLD);
+  std::vector<std::array<abm::graph::vertex_t, 2>> od_pairs(chunk_size);
+
+  // Send route chunks to different compute nodes
+  MPI_Scatter(all_od_pairs_.data(), chunk_size, pair_t, od_pairs.data(),
+              od_pairs.size(), pair_t, 0, MPI_COMM_WORLD);
+  // Calculate the remaining chunk of od_pairs and add to rank 0
+  int chunk_remainder = all_od_pairs_.size() % mpi_size;
+  if (mpi_rank == 0) {
+    od_pairs.insert(od_pairs.begin(), all_od_pairs_.end() - chunk_remainder,
+                    all_od_pairs_.end());
+  }
+
+  // Paths (vector of edges)
+  std::vector<std::array<abm::graph::vertex_t, 2>> paths;
+  paths.reserve(graph_->nedges());
+
+  // Indices of start of path and length for each agent
+  std::vector<std::array<abm::graph::vertex_t, 3>> paths_idx;
+  paths_idx.reserve(od_pairs.size());
+
+#pragma omp parallel for schedule(dynamic)
+  for (abm::graph::vertex_t i = 0; i < od_pairs.size(); ++i) {
+    const auto sp = graph_->dijkstra(od_pairs[i][0], od_pairs[i][1]);
+#pragma omp critical
+    {
+      paths_idx.emplace_back(std::array<abm::graph::vertex_t, 3>(
+          {i, static_cast<abm::graph::vertex_t>(paths.size()),
+           static_cast<abm::graph::vertex_t>(sp.size())}));
+      paths.insert(std::end(paths), std::begin(sp), std::end(sp));
+    }
+  }
+
+  if (mpi_rank == 0) {
+    // Get all paths and indices
+    this->all_paths_ = abm::gather_vector_arrays(paths);
+    const auto all_paths_idx = abm::gather_vector_arrays(paths_idx);
+  }
+  return all_paths_;
 }
